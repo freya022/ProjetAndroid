0deec2cbcc5547e9bac9195b415bd1d0e8262e07
freya022 <41875020+freya022@users.noreply.github.com>
2022-03-01 15:20:42 +0100

    Fichiers de base

---
+++ app/src/main/java/com/freya02/projetandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.freya02.projetandroid;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
